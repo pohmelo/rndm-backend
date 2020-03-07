b698c940ff61bf997c2a22348ba9c1a4a869d7ad
Mikko <mikkovp@hotmail.com>
2020-03-07 10:37:22 +0200

    Initial commit

---
+++ src/main/java/com/pohmelo/rndm/RndmApplication.java
@@ -0,0 +1,14 @@
+package com.pohmelo.rndm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RndmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RndmApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
---
+++ src/test/java/com/pohmelo/rndm/RndmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pohmelo.rndm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RndmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
